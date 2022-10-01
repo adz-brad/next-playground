8eccf439245984a4c2026db9ea446d07de906003
Brad S <brad@adrenalizedigital.ca>
2022-10-01 11:13:53 -0600

    update postmark api

--- pages/api/forms.js
+++ pages/api/forms.js
@@ -1,50 +1,19 @@
 import { ServerClient } from 'postmark'
 
-export default async function (req, res) {
+export default async function (req) {
   const serverToken = process.env.POSTMARK_API_TOKEN;
   const client = new ServerClient(serverToken);
-  const submit = new Promise(resolve => {
-    client.sendEmail({
-      "From": process.env.POSTMARK_EMAIL,
-      "To": process.env.POSTMARK_EMAIL,
-      "ReplyTo": req.body.email,
-      "TextBody": "Hello dear Postmark user.",  
-      }).then(function(res) {
-        if(res.ErrorCode === 0){
-          // Add backend action for successful form submission or remove
-          // console.log('API Response:', res)
-          return res
-        }
-        else {
-          // Add backend action for unsuccessful form submission or remove
-          // console.log('API Response Error:', res)
-          return res
-        }
-    }, function(err) {
-        // Do something with error response or remove
-        // console.error('API Error:', err)
-        return err
-    });
-    res.end();
-    resolve()
+  const submit = await client.sendEmail({
+    "From": process.env.POSTMARK_EMAIL,
+    "To": process.env.POSTMARK_EMAIL,
+    "ReplyTo": req.body.email,
+    "TextBody": "Hello dear Postmark user.",  
+  }).then(function(res) {
     return res
+  }, function(err) {
+    return err
   })
-  return submit
+    return submit
 }
 
-// export default async function (req) {
-//   const serverToken = process.env.POSTMARK_API_TOKEN;
-//   const client = new ServerClient(serverToken);
-//   const submit = await client.sendEmail({
-//     "From": process.env.POSTMARK_EMAIL,
-//     "To": process.env.POSTMARK_EMAIL,
-//     "ReplyTo": req.body.email,
-//     "TextBody": "Hello dear Postmark user.",  
-//   }).then(function(res) {
-//     return res
-//   }, function(err) {
-//     return err
-//   })
-//     return submit
-// }
 
